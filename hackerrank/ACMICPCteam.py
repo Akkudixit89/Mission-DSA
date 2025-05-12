66e32d89c6538d152e0df5a464470adba9c7c5ec
Akanksha Dixit <2k23.cscys2310306@gmail.com>
2025-05-12 18:16:38 +0530

    Update ACMICPCteam.py

--- hackerrank/ACMICPCteam.py
+++ hackerrank/ACMICPCteam.py
@@ -1,1 +1,11 @@
+from itertools import combinations
 
+def acmTeam(topic):
+    # Write your code here
+    
+    r = [ {i+1 for i,j in enumerate(s) if j == "1"} for s in topic]
+         
+    x = [ list(u[0].union(u[1])) for u in combinations(r, r=2) ]
+    x = list(map(lambda i: len(i), x))
+    
+    return [max(x), x.count(max(x))] 
