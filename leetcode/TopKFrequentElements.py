1a766fdeee6e35032f8cf3722b9877dce13d1f93
Akanksha Dixit <2k23.cscys2310306@gmail.com>
2025-05-12 16:48:20 +0530

    Update TopKFrequentElements.py

--- leetcode/TopKFrequentElements.py
+++ leetcode/TopKFrequentElements.py
@@ -1,1 +1,19 @@
+from collections import Counter
 
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        d = Counter(nums)
+        # Sort the dictionary by frequency in descending order
+        sorted_items = sorted(d.items(), key=lambda item: item[1], reverse=True)
+        
+        # Extract the top k elements
+        l = []
+        for i in range(k):
+            l.append(sorted_items[i][0])
+        
+        return l
